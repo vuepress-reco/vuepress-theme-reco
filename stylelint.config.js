5dcde9c421fce2c6283477943991111323379de2
reco_luan <recoluan@outlook.com>
2021-05-10 00:05:59 +0800

    build: add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+    'declaration-block-no-duplicate-custom-properties': false,
+    'named-grid-areas-no-invalid': null,
+    'no-irregular-whitespace': false,
+    'no-invalid-position-at-import-rule': null,
+  },
+}
