70a457fb19d62c6ab7ec8254e132ea2edffc8f60
reco_luan <recoluan@outlook.com>
2021-05-15 00:43:39 +0800

    feat(style-default): code style & md style

--- packages/@vuepress-reco/style-default/src/client/components/Page.vue
+++ packages/@vuepress-reco/style-default/src/client/components/Page.vue
@@ -1,5 +1,9 @@
 <template>
-  <main class="page-container pl-80 mt-14">
-    <Content />
+  <main class="page-container pl-80 mt-24">
+    <div
+      class="theme-default-content lg:max-w-screen-md xl:max-w-screen-lg m-auto px-10"
+    >
+      <Content />
+    </div>
   </main>
 </template>
--- packages/@vuepress-reco/style-default/src/node/tailwind/config.ts
+++ packages/@vuepress-reco/style-default/src/node/tailwind/config.ts
@@ -5,13 +5,6 @@ export const tailwindConfig = {
   purge: ['./src/**/*.vue'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    screens: {
-      'sm': '640px',
-      'md': '1024px',
-      'lg': '1280px',
-      'xl': '1536px',
-      '2xl': '1880px',
-    },
     extend: {
       height: (): Record<string, any> => ({ 'screen-3/5': '60vh' }),
       colors: {
