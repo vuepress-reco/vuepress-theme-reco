33862bdacf5cee2d9bb23426d5ea3b7338efa806
reco_luan <recoluan@outlook.com>
2021-06-10 01:11:03 +0800

    fix: style

--- packages/vuepress-theme-reco/src/index.ts
+++ packages/vuepress-theme-reco/src/index.ts
@@ -4,10 +4,9 @@ import { path } from '@vuepress/utils'
 export const recoTheme: Theme<ThemeConfig> = (themeConfig: ThemeConfig) => {
   const { style } = themeConfig
   const stylePath = path.resolve(process.cwd(), `node_modules/${style}`)
-  const getStyleConfig = require(path.resolve(`${stylePath}/lib/index.js`))
-    .default
-
-  console.log(555, getStyleConfig(themeConfig))
+  const getStyleConfig = require(path.resolve(
+    `${stylePath}/lib/index.js`
+  )).default
 
   return {
     name: 'vuepress-theme-reco',
