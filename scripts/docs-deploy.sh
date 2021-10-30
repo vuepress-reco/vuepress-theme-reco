17515f073e2ad937387751d92c021f60755f8f76
reco_luan <recoluan@outlook.com>
2021-10-30 20:38:35 +0800

    docs: deploy sh

--- scripts/docs-deploy.sh
+++ scripts/docs-deploy.sh
@@ -1,16 +1,24 @@
 npm run build:docs
 
-cd public
+cd docs/.vuepress/dist
+
+ls
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'docs: update docs'
 
 # git push -f git@github.com:recoluan/vuepress-reco-doc.git master
 
 # git push -f git@git.coding.net:recoluan/vuepress-theme-reco-doc.git master
 
-git push -f git@github.com:vuepress-reco/vuepress-reco.github.io.git master
+git remote remove origin
+
+git remote add origin git@github.com:vuepress-reco/docs-v2.git
+
+git remote -v
+
+git push origin master -f
 
 cd ../
-rm -rf public
+rm -rf dist
