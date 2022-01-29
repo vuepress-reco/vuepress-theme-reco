ff2af4a2e724e08facda2a0a6e80ea37beeb2c16
reco_luan <recoluan@outlook.com>
2022-01-29 16:44:01 +0800

    fix: fix vue preview display bug

--- packages/@vuepress-reco/style-default/src/client/components/global/VuePreview.vue
+++ packages/@vuepress-reco/style-default/src/client/components/global/VuePreview.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vue-preview-container">
-    <div class="preview-container">
-      <component v-if="component" :is="component" />
+    <div class="preview-container" v-if="component !== undefined">
+      <component :is="component" />
     </div>
     <slot></slot>
   </div>
