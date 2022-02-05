d8b7c010a51f301048ee7c929309779a2c4f3ab2
reco_luan <recoluan@outlook.com>
2022-02-05 22:08:03 +0800

    chore: remove test code

--- packages/@vuepress-reco/plugin-bulletin-popover/src/node/bulletinPopoverPlugin.ts
+++ packages/@vuepress-reco/plugin-bulletin-popover/src/node/bulletinPopoverPlugin.ts
@@ -1,11 +1,6 @@
 import type { Plugin, App } from '@vuepress/core'
 import { path } from '@vuepress/utils'
 
-console.log(222, path.resolve(
-  __dirname,
-  '../client/components/Bulletin.vue'
-))
-
 export const bulletinPopoverPlugin: Plugin<Record<string, any>> = (
   options,
   app: App
