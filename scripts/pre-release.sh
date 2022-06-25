f7aa98080531d01ca12d4adb1f5a9319b6d07517
reco_luan <recoluan@qq.com>
2022-06-25 18:17:18 +0800

    buld: modify bash pre-release

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -9,6 +9,9 @@ pnpm changeset
 # 提升版本
 pnpm changeset version
 
+# 关闭 Prereleases 模式
+pnpm changeset pre exit
+
 # 新增版本 commit# get new version
 VERSION=`node -p "require('./packages/vuepress-theme-reco/package.json').version"`
 git add .
@@ -21,6 +24,3 @@ git push origin "v${VERSION}"
 
 # 发布到 dist-tag next 下
 pnpm changeset publish --tag next
-
-# 关闭 Prereleases 模式
-pnpm changeset pre exit
