8bad7cfcfbde47bb28e81fa4352f0caeb007e480
reco_luan <recoluan@qq.com>
2022-07-11 00:43:03 +0800

    feat: add 404 page

--- packages/vuepress-theme-reco/src/client/layouts/404.vue
+++ packages/vuepress-theme-reco/src/client/layouts/404.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>404</div>
+  <Common>
+    <section class="page-404-wrapper">
+      <div class="content">
+        <h1>404</h1>
+        <p>Oops! Page does not exist.</p>
+        <div class="icon-container" @click="goHome">Go Home</div>
+      </div>
+      <img src="../assets/taken.svg" />
+    </section>
+  </Common>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import Common from '../components/Common/index.vue'
+
+const router = useRouter()
+
+const goHome = () => {
+  router.push('/')
+}
+</script>
+
