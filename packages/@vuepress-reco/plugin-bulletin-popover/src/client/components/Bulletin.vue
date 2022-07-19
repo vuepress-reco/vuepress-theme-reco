a5533b126297772e8bd0184c55fe91560be704bb
reco_luan <recoluan@qq.com>
2022-07-20 00:13:59 +0800

    fix:  optimize the style

--- packages/@vuepress-reco/plugin-bulletin-popover/src/client/components/Bulletin.vue
+++ packages/@vuepress-reco/plugin-bulletin-popover/src/client/components/Bulletin.vue
@@ -30,7 +30,7 @@ const { bodyNodes } = useHandleNodes()
 @import '@vuepress-reco/tailwindcss-config/lib/client/styles/tailwindcss-base.css';
 
 .bulletin-wrapper {
-  @apply fixed top-20 right-4 z-10 box-border border-primary rounded bg-block;
+  @apply fixed top-20 right-4 z-10 box-border border-primary rounded bg-basic;
   min-height: 200px;
   max-height: 660px;
   overflow-y: auto;
