88f636b5abd97ad179b05cb1ffaec21058eb5b61
senmu <wb_qinxiaosen@meituan.com>
2022-07-20 20:32:56 +0800

    feat: Add autoload dark mode

--- packages/vuepress-theme-reco/src/client/components/ToggleDarkModeButton.vue
+++ packages/vuepress-theme-reco/src/client/components/ToggleDarkModeButton.vue
@@ -4,29 +4,39 @@
 
 <script setup lang="ts">
 import { ref, computed, onMounted, watch } from 'vue'
+const APPEARANCE_KEY = 'vuepress-reco-color-scheme'
+let userPreference = localStorage[APPEARANCE_KEY] || 'auto'
+
+const query = window.matchMedia('(prefers-color-scheme: dark)')
+const classList = document.documentElement.classList
+
+query.onchange = (e) => {
+  if (userPreference === 'auto') {
+    setClass((isDarkMode.value = e.matches))
+  }
+}
 const isDarkMode = ref(false)
 const icon = computed(() => {
   return isDarkMode.value ? 'Sun' : 'MoonStars'
 })
-
 let toggleMode = () => {
   isDarkMode.value = !isDarkMode.value
 }
-
+function setClass(dark: boolean): void {
+  classList.toggle('dark', dark)
+}
 onMounted(() => {
   watch(isDarkMode, (m) => {
-    localStorage['vuepress-reco-color-scheme'] = m ? 'dark' : 'light'
-    document.documentElement.classList.toggle('dark', isDarkMode.value)
+    localStorage[APPEARANCE_KEY] = m
+      ? query.matches ? 'auto' : 'dark'
+      : query.matches ? 'light' : 'auto'
+    setClass(isDarkMode.value)
   })
-
   const initMode = () => {
-    if ('vuepress-reco-color-scheme' in localStorage) {
-      isDarkMode.value = localStorage['vuepress-reco-color-scheme'] === 'dark'
-    } else {
-      isDarkMode.value = window.matchMedia('(prefers-color-scheme: dark)').matches
-    }
+    isDarkMode.value = userPreference === 'auto'
+      ? query.matches
+      : userPreference === 'dark'
   }
-
   initMode()
 })
 </script>
