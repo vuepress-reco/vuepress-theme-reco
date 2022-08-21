1d4049e69c1f6084e05001f7b25b25be94541cae
senmu <wb_qinxiaosen@meituan.com>
2022-08-21 18:03:53 +0800

    feat: add custom viteBundler configuration

--- packages/vuepress-theme-reco/src/node/resolveBundlerConfig.ts
+++ packages/vuepress-theme-reco/src/node/resolveBundlerConfig.ts
@@ -1,10 +1,31 @@
 import { tailwindcssConfig } from '@vuepress-reco/tailwindcss-config'
 import type { ViteBundlerOptions } from '@vuepress/bundler-vite'
 
-export const mergeViteBundlerConfig = (options: ViteBundlerOptions, config: ViteBundlerOptions): ViteBundlerOptions => ({
-  viteOptions: { ...(options?.viteOptions || {}), ...(config?.viteOptions || {}) },
-  vuePluginOptions: { ...(options?.vuePluginOptions || {}), ...(config?.vuePluginOptions || {}) }
-})
+export const mergeViteBundlerConfig = (options: ViteBundlerOptions, config: ViteBundlerOptions): ViteBundlerOptions => {
+  // Handling postcss alone
+  // For inline PostCSS config, it expects the same format as postcss.config.js.
+  // But for plugins property, only array format can be used.
+  const userViteConfig = config?.viteOptions || {}
+  if (typeof userViteConfig?.css?.postcss === 'string') throw new Error('String type postcss is not supported yet')
+  if (!Array.isArray(userViteConfig?.css?.postcss?.plugins)) throw new Error('plugins for postcss must be an array')
+  const userPostcssPlugins = userViteConfig?.css?.postcss?.plugins || []
+
+  const viteOptions = {
+    css: {
+      postcss: {
+        plugins: [...(options?.viteOptions?.css?.postcss as any)?.plugins, ...userPostcssPlugins],
+        ...userViteConfig?.css?.postcss
+      },
+      ...userViteConfig?.css?.postcss
+    },
+    ...userViteConfig
+  }
+
+  return {
+    viteOptions: { ...viteOptions },
+    vuePluginOptions: { ...(options?.vuePluginOptions || {}), ...(config?.vuePluginOptions || {}) }
+  }
+}
 
 export const defaultViteBundlerConfig = (): ViteBundlerOptions => ({
   viteOptions: {
