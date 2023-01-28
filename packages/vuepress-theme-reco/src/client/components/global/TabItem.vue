8405615065a7715c56737180185acb15951b01b4
Reco <recoluan@qq.com>
2023-01-28 11:57:04 +0800

    Create TabItem.vue

---
+++ packages/vuepress-theme-reco/src/client/components/global/TabItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="tab-item"
+    :class="{ 'tab-item__active': active }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'TabItem',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  }
+})
+</script>
+
+<style>
+@import url('@vuepress-reco/tailwindcss-config/lib/client/styles/tailwindcss-base.css');
+
+.tab-item {
+  @apply hidden border-block border-0 border-t border-dashed p-4;
+  margin-top: -1px;
+  &.tab-item__active {
+    @apply block;
+  }
+  div[class*="language-"] {
+    @apply border-none;
+    &::after {
+      @apply hidden;
+    }
+    pre[class*="language-"] {
+      @apply mb-0 pt-0 pb-4 border-t-0 !important;
+    }
+  }
+  .line-numbers {
+    @apply pb-4 !important;
+  }
+}
+</style>
