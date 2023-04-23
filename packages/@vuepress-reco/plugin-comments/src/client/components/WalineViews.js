08a70347da31e60aff2cb56a9a3c702c09fe85ec
reco_luan <recoluan@qq.com>
2023-04-23 23:42:37 +0800

    fix: fix the bug of waline

--- packages/@vuepress-reco/plugin-comments/src/client/components/WalineViews.js
+++ packages/@vuepress-reco/plugin-comments/src/client/components/WalineViews.js
@@ -1,33 +1,34 @@
-import { defineComponent, h, onMounted, onUpdated, toRefs } from 'vue';
-import { pageviewCount } from '@waline/client/src/pageview';
-import { useComment } from '../composables';
-import { useRoute } from 'vue-router';
+import { defineComponent, h, onMounted, onUpdated, toRefs } from 'vue'
+import { pageviewCount } from '@waline/client/pageview'
+import { useComment } from '../composables'
+import { useRoute } from 'vue-router'
 export default defineComponent({
-    name: 'WalineViews',
-    props: {
-        path: String,
-    },
-    setup(props) {
-        const { path } = toRefs(props);
-        const { options } = useComment();
-        const route = useRoute();
-        const viewFn = function() {
-          pageviewCount({
-            serverURL: options.value.serverURL,
-            path: path.value,
-            selector: `[data-path="${path.value}"]`,
-            update: route.path === path.value,
-          });
-        }
-        onMounted(() => {
-          viewFn();
-        });
-        onUpdated(() => {
-          viewFn()
-        })
-        return () => h('span', {
-            class: 'waline-pageview-count',
-            'data-path': path.value
-        });
+  name: 'WalineViews',
+  props: {
+    path: String,
+  },
+  setup(props) {
+    const { path } = toRefs(props)
+    const { options } = useComment()
+    const route = useRoute()
+    const viewFn = function () {
+      pageviewCount({
+        serverURL: options.value.serverURL,
+        path: path.value,
+        selector: `[data-path="${path.value}"]`,
+        update: route.path === path.value,
+      })
     }
-});
+    onMounted(() => {
+      viewFn()
+    })
+    onUpdated(() => {
+      viewFn()
+    })
+    return () =>
+      h('span', {
+        'class': 'waline-pageview-count',
+        'data-path': path.value,
+      })
+  },
+})
