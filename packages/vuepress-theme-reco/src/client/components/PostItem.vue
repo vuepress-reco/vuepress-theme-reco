5bdde7d591247f130737e75acedc1080749ef31a
wangshiting <stwangcq@163.com>
2023-04-25 15:32:24 +0800

    fix(theme-reco): remove the pageview of valine from the post list

--- packages/vuepress-theme-reco/src/client/components/PostItem.vue
+++ packages/vuepress-theme-reco/src/client/components/PostItem.vue
@@ -3,12 +3,13 @@
     <div class="title">
       <RouterLink :to="data.path">{{ data.title }}</RouterLink>
     </div>
-    <PageInfo :page-data="data" :hide-views="false"> </PageInfo>
+    <PageInfo :page-data="data" :hide-views="solution==='valine'"> </PageInfo>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useComment } from '@vuepress-reco/vuepress-plugin-comments/lib/client/composables'
 import PageInfo from './PageInfo.vue'
 
 export default defineComponent({
@@ -22,7 +23,8 @@ export default defineComponent({
   },
 
   setup() {
-    return {}
+    const { solution } = useComment()
+    return { solution }
   },
 })
 </script>
