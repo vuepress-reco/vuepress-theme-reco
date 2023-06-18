78673c050ef70f252edb491b491389fa4f9fd426
reco_luan <recoluan@qq.com>
2023-06-19 00:57:53 +0800

    fix: sort bug of repair series
    ISSUES CLOSED: #206

--- packages/vuepress-theme-reco/src/client/components/Series.vue
+++ packages/vuepress-theme-reco/src/client/components/Series.vue
@@ -7,16 +7,21 @@
 
 <script lang="ts" setup>
 import { computed } from 'vue';
+import { useThemeLocaleData } from '@vuepress/plugin-theme-data/client'
 import { useSeriesItems, useSortSeries } from '../composables'
 import { SeriesItem } from './SeriesItem'
 import SiteBrand from './SiteBrand.vue'
 
+const themeLocal = useThemeLocaleData()
 const { sortSeries } = useSortSeries()
 const seriesItems = useSeriesItems()
 
 const sortedSeries = computed(() => {
-  const series = sortSeries(seriesItems.value)
+  if (!themeLocal.value.autoSetSeries) {
+    return seriesItems.value
+  }
 
+  const series = sortSeries(seriesItems.value)
   return series
 })
 </script>
