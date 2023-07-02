09c04e59696898fbb4258ff9095b28011923a5ae
reco_luan <recoluan@qq.com>
2023-07-02 20:55:32 +0800

    feat: add friendship link page

--- packages/vuepress-theme-reco/src/client/config.ts
+++ packages/vuepress-theme-reco/src/client/config.ts
@@ -1,14 +1,9 @@
-import { h } from 'vue'
 import { defineClientConfig } from '@vuepress/client'
 
 import { applyClientSetup } from './clientSetup'
 import { applyClientEnhance } from './clientEnhance'
 
-import Categories from './layouts/Categories.vue'
-import Layout from './layouts/Layout.vue'
-import NotFound from './layouts/NotFound.vue'
-import Posts from './layouts/Posts.vue'
-import Timeline from './layouts/Timeline.vue'
+import * as layouts from './layouts'
 
 export default defineClientConfig({
   enhance(...args) {
@@ -17,11 +12,5 @@ export default defineClientConfig({
   setup() {
     applyClientSetup()
   },
-  layouts: {
-    Categories: h(Categories),
-    Layout: h(Layout),
-    NotFound: h(NotFound),
-    Posts: h(Posts),
-    Timeline: h(Timeline),
-  },
+  layouts,
 })
