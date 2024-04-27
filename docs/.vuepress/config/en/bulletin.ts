73609fcf55e4f69baf7de1f65f04c57362dd8437
reco_luan <recoluan@qq.com>
2024-04-27 23:44:48 +0800

    docs: update docs

--- docs/.vuepress/config/en/bulletin.ts
+++ docs/.vuepress/config/en/bulletin.ts
@@ -3,30 +3,22 @@ export const bulletin = {
   body: [
     {
       type: 'title',
-      content: 'Latest',
+      content: 'QQ Channel：3u8x6485s0',
+    },
+    {
+      type: 'image',
+      src: '/qq_channel.png',
     },
     {
       type: 'text',
-      content: `
-      <div>
-        <div>vuepress@2.0.0-rc.9</div>
-        <div>vuepress-theme-reco@2.0.0-rc.8</div>
-      </div>`,
-      style: 'font-size: 12px;'
-    },
-    {
-      type: 'title',
-      content: 'QQ Group',
-    },
-    {
-      type: 'text',
-      content: `
-      <ul>
-        <li>QQ Group 1：1037296104</li>
-        <li>QQ Group 2：1061561395</li>
-        <li>QQ Group 3：962687802</li>
-      </ul>`,
-      style: 'font-size: 12px;'
+      // content: `
+      // <ul>
+      //   <li>QQ Group 1：1037296104</li>
+      //   <li>QQ Group 2：1061561395</li>
+      //   <li>QQ Group 3：962687802</li>
+      // </ul>`,
+      content: `<br /><div><b>Tip: </b>The original three QQ group members are quite different and scattered, so new people are stopped to join the group now. In order to facilitate your communication, please move to the QQ channel.</div>`,
+      style: 'font-size: 12px; color: red;'
     },
     {
       type: 'hr',
