e688ae9779c658a858c5e37687317ae44fa167c8
yxzlwz <yxzlwz@gmail.com>
2025-10-29 21:23:25 +0800

    remove demo

--- docs/.vuepress/data/examples.ts
+++ docs/.vuepress/data/examples.ts
@@ -68,11 +68,6 @@ export default [
     avator: "https://www.licodeao.top/avatar.jpg"
   },
   {
-    thumbnail: "https://github-production-user-asset-6210df.s3.amazonaws.com/75941562/241768717-21953f79-fe88-4dca-bfaf-306d4a8c1dac.png",
-    link: "https://www.yixiangzhilv.com/",
-    avator: "https://www.yixiangzhilv.com/head.png"
-  },
-  {
     thumbnail: "https://blog.latteandcat.cn/bg.jpg",
     link: "https://blog.latteandcat.cn/",
     avator: "https://blog.latteandcat.cn/head.png"
