6c2c1475ecba5576f615d0cc0ec30f9e5d409d5d
reco_luan <recoluan@qq.com>
2026-07-22 19:00:50 -0700

    fix: restore docs deployment

--- scripts/docs-deploy.sh
+++ scripts/docs-deploy.sh
@@ -1,10 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 pnpm docs:build
 
 cd docs/.vuepress/dist
 
 ls
 
-git init
+git init -b master
 git add -A
 git commit -m 'docs: update docs'
 
@@ -12,13 +16,13 @@ git commit -m 'docs: update docs'
 
 # git push -f git@git.coding.net:recoluan/vuepress-theme-reco-doc.git master
 
-git remote remove origin
+git remote remove origin 2>/dev/null || true
 
 git remote add origin git@github.com:vuepress-reco/vuepress-reco.github.io.git
 
 git remote -v
 
-git push origin master -f
+git push --force origin master
 
 cd ../
 rm -rf dist
